dd157aad1c776e687e10d5aa66c241d043c074ae
Meir Amara <meir757@gmail.com>
2023-05-11 11:01:58 +0300

    11/5/2023

---
+++ src/components/catCard/CatCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CatCard.scss";
+
+export default function CatCard() {
+  return (
+    <div className="catCard">
+      <div className="container">
+        
+      </div>
+    </div>
+  );
+}
---
+++ src/components/slide/Slide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Slide.scss";
+
+export default function Slide() {
+  return (
+    <div className="slide">
+      <div className="container">
+        
+      </div>
+    </div>
+  );
+}
